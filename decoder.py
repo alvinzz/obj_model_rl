121a0cc50f37a655ba553c2b3cf43628724cbff5
alvinzz <alvinz@berkeley.edu>
2018-10-26 17:47:41 -0700

    multiobject autoencoder testing (objects too similar)

--- decoder.py
+++ decoder.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from gumbel_softmax import gumbel_softmax_sample
+from tqdm import tqdm
 
 class Decoder(nn.Module):
     def __init__(self, channels=[9, 36, 36, 3], obj_classes=2):
@@ -81,15 +82,15 @@ if __name__ == '__main__':
     kl_weight = 1.0
     reconstr_weight = 1.0
     learning_rate = 0.001
-    mb_size = 20
+    mb_size = 64
 
     use_cuda = torch.cuda.is_available()
     torch.manual_seed(0)
     np.random.seed(0)
     device = torch.device("cuda" if use_cuda else "cpu")
 
-    enc = Encoder().to(device)
-    dec = Decoder().to(device)
+    enc = Encoder([3,36,36,3], [0,0,0,3], 3).to(device)
+    dec = Decoder([9,36,36,3], 3).to(device)
     params = {}
     for (k, v) in enc.named_parameters():
         params['enc.'+k.replace('__', '.')] = v
@@ -99,24 +100,22 @@ if __name__ == '__main__':
 
     data = h5py.File('data/obj_balls.h5', 'r')
     print('extracting datasets to numpy...')
-    train_data = data['training']['features'][()]
-    val_data = data['validation']['features'][()]
+    train_data = data['training']['features'][:1,:]
+    val_data = data['validation']['features'][:1,:]
     print('done!')
-    train_data_len = data['training']['features'].shape[1]
-    val_data_len = data['validation']['features'].shape[1]
 
-    prior = np.array([(64*64-4)/(64*64.), 4/(64*64.)]).astype(np.float32)
+    prior = np.array([(64*64-4)/(64*64.), 2/(64*64.), 2/(64*64.)]).astype(np.float32)
     prior = np.reshape(prior, [1, -1, 1, 1])
     prior = torch.tensor(np.tile(prior, [1, 1, 64, 64]), device=device)
 
     eps = 1e-20
     enc.train()
     dec.train()
-    for epoch in range(100):
-        start_inds = np.arange(train_data_len // mb_size)
-        np.random.shuffle(start_inds)
-        for start_ind in start_inds:
-            ims = train_data[0,start_ind*mb_size:(start_ind+1)*mb_size,:,:,0] - 0.5, [3,1,1,1]).transpose([1,0,2,3]).astype(np.float32)
+    for epoch in range(10): #30
+        mb_inds = np.arange(train_data.shape[1])
+        np.random.shuffle(mb_inds)
+        for mb in tqdm(range(len(mb_inds) // mb_size)):
+            ims = np.tile(train_data[0,mb_inds[mb*mb_size:(mb+1)*mb_size],:,:,0] - 0.5, [3,1,1,1]).transpose([1,0,2,3]).astype(np.float32)
             ims_tensor = torch.tensor(ims, device=device)
 
             latent = enc(ims_tensor)
@@ -133,16 +132,15 @@ if __name__ == '__main__':
             reconstr_loss = torch.mean(
                 (ims_tensor - reconstr)**2
             )
-            kl_weight = (epoch-10)/9. if epoch > 10 else 0
+            kl_weight = epoch / 3.
             loss = kl_weight*kl_loss + reconstr_weight*reconstr_loss
-            print(loss.detach().cpu().numpy())
             loss.backward()
             optimizer.step()
         if epoch % 1 == 0:
             print(epoch, kl_weight*kl_loss.detach().cpu().numpy(), reconstr_weight*reconstr_loss.detach().cpu().numpy())
     print(epoch, kl_weight*kl_loss.detach().cpu().numpy(), reconstr_weight*reconstr_loss.detach().cpu().numpy())
 
-    val_inds = np.arange(val_data_len)
+    val_inds = np.arange(val_data.shape[1])
     np.random.shuffle(val_inds)
     for val_ind in val_inds[:5]:
         ims = np.tile(val_data[0,val_ind:val_ind+1,:,:,0] - 0.5, [3,1,1,1]).transpose([1,0,2,3]).astype(np.float32)
@@ -155,8 +153,10 @@ if __name__ == '__main__':
         ).permute(0, 3, 1, 2)
         reconstr = dec(samples)
 
-        latent_im = latent.detach().cpu().numpy()[0, 0]
-        samples_im = samples.detach().cpu().numpy()[0, 0]
+        latent_im = (latent.detach().cpu().numpy()[0, :]).transpose([1,2,0])
+        import pickle
+        pickle.dump(latent_im, open('data/latent_{}.pkl'.format(val_ind), 'wb'))
+        samples_im = (samples.detach().cpu().numpy()[0, :]).transpose([1,2,0])
         input_im = ims_tensor.detach().cpu().numpy()[0, 0]+0.5
         reconstr_im = reconstr.detach().cpu().numpy()[0, 0]+0.5
         cv2.imwrite('data/latent_{}.png'.format(val_ind), (255*np.clip(latent_im, 0, 1)).astype(np.uint8))
