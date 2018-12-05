b71bac63bd6f5be5913035f176c39b949135a367
Ubuntu <ubuntu@ip-172-31-39-130.us-west-2.compute.internal>
2018-12-05 15:53:23 +0000

    binary gs

--- gumbel_softmax.py
+++ gumbel_softmax.py
@@ -5,11 +5,11 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
 
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-else:
-    device = torch.device("cpu")
-
+#if torch.cuda.is_available():
+#    device = torch.device("cuda")
+#else:
+#    device = torch.device("cpu")
+device = torch.device("cpu")
 
 def sample_gumbel(shape, eps=1e-20):
     U = torch.rand(shape, dtype=torch.float32, device=device)
@@ -18,6 +18,13 @@ def sample_gumbel(shape, eps=1e-20):
 def gumbel_softmax_sample(logits, temperature, eps=1e-20):
     y = torch.log(logits + eps) + sample_gumbel(logits.size())
     return F.softmax(y / temperature, dim=-1)
+
+def binary_gumbel_softmax_sample(logits, temperature, eps=1e-20):
+    y = logits.reshape(logits.shape+(1,))
+    y = torch.cat((y, 1-y), dim=-1)
+    sample = gumbel_softmax_sample(y, temperature)
+    #TODO: generalize to different shapes
+    return sample[:,:,:,:,0]
 
 def hard_gumbel_softmax_sample(logits, temperature, eps=1e-20):
     """
@@ -37,5 +44,6 @@ if __name__ == '__main__':
     logits = np.reshape(np.tile(logits, 1000*4), (1000, 5, 2, 2))
     print(logits[0])
     for temperature in [0.01]:
-        samples = np.transpose(gumbel_softmax_sample(Variable(torch.Tensor(np.transpose(logits, [0,2,3,1]), device=device)), temperature).cpu().numpy(), [0,3,1,2])
+        #samples = np.transpose(gumbel_softmax_sample(Variable(torch.Tensor(np.transpose(logits, [0,2,3,1]), device=device)), temperature).cpu().numpy(), [0,3,1,2])
+        samples = np.transpose(binary_gumbel_softmax_sample(Variable(torch.Tensor(np.transpose(logits, [0,2,3,1]), device=device)), temperature).cpu().numpy(), [0,3,1,2])
         print(np.sum(samples, axis=0))
